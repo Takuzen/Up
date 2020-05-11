282a78a6d0871bcd4c2ac935ce1c49036983c40d
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-05-11 17:22:36 +0900

    名前変更

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
 #  タイトル；タイトルタグで使用
 admin.site.site_title = 'タイトル'
 #  サイト名：ログイン画面と管理画面上部の表示
-admin.site.site_header = 'サンプルアプリ'
+admin.site.site_header = 'UP'
 #  メニュー：管理画面の見出し表示
 admin.site.index_title = 'メニュー'
