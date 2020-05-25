51ce21535f4b539c79863081fa2d63402d317c38
Haruaki Tanaka <optubrx566@gmail.com>
2020-05-25 18:03:12 +0900

    removed useless part

--- app/urls.py
+++ app/urls.py
@@ -24,8 +24,6 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
