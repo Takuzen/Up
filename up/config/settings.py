f9506734a8132f01ba538bdc3dd931a06e11ffed
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-05-28 11:32:37 +0900

    docker環境構築

--- up/config/settings.py
+++ up/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_(u)&e-3076d7_a(gfhb85*)u-u4vwc%#qt7c7q3-t98%lvp&g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 # Application definition
 
