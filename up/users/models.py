75e6e9a20e44bb1c9219932ed578c7ecd9327aef
reishim <44530189+greenteabiscuit@users.noreply.github.com>
2020-06-08 18:47:48 +0900

    Revert "Features/improve mypage"

--- up/users/models.py
+++ up/users/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
         if self.full_name:
             return self.full_name
         else:
-            return self.username
+            return self.username + '（氏名未登録）'
 
     # 選択リストでの表示
     def __str__(self):
