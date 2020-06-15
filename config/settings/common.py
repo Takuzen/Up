38ab591026c76e3c5cc5386135b012e0fe96b2ef
Takuzen Tou <takuzen@takuzens-mbp.lan>
2020-06-16 00:15:17 +0900

    changed media_root to resolve conflict

--- config/settings/common.py
+++ config/settings/common.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -174,7 +175,7 @@ LOGGING = {
     }
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'up/media')
-MEDIA_URL = 'up/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'up/app/media')
+MEDIA_URL = 'media/'
 
 INTERNAL_IPS = ['127.0.0.1']
