afa5f84e81e205a87123d8300a24b1b23f9eb5c5
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-07-28 22:37:45 +0900

    delete admin page path

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 # ルーティング設定
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('up.app.urls')),
 ]
 
