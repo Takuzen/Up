99821ed15e14cc327ef9f0a39ca352cb43e37a74
takuzen <takuzen0430@gmail.com>
2020-08-15 23:28:11 +0900

    changed debug from true to false so I can check 404/500 page layout, and made 404.html under templates

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,7 @@
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
@@ -11,16 +11,17 @@ ALLOWED_HOSTS = ["0.0.0.0"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'Up', #←.env_sampleで指定したMYSQL_DATABASE
-        'USER' : 'db_user', #←.env_sampleで指定したMYSQL_USER
-        'PASSWORD' : 'password', #←.env_sampleで指定したMYSQL_PASSWORD
-        'HOST' : '172.20.0.3', #←docker-compose.ymlに指定したmysqlのIPAddress
+        'NAME': 'Up',  # ←.env_sampleで指定したMYSQL_DATABASE
+        'USER': 'db_user',  # ←.env_sampleで指定したMYSQL_USER
+        'PASSWORD': 'password',  # ←.env_sampleで指定したMYSQL_PASSWORD
+        'HOST': '172.20.0.3',  # ←docker-compose.ymlに指定したmysqlのIPAddress
         'PORT': '3306',
     }
 }
 
+
 def show_toolbar(request):
-        return True
+    return True
 
 
 DEBUG_TOOLBAR_CONFIG = {
