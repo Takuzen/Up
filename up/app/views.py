1cef8e798919cbee6b3d8bdc65edb9d2ce1f9316
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-09-11 21:50:41 +0900

    createから投稿可能

--- up/app/views.py
+++ up/app/views.py
@@ -113,7 +113,6 @@ class ItemFilterView(FilterView):
         image_form = ImageForm(request.FILES)
         if postForm.is_valid():
             item = postForm.save(commit=False)
-            item.image = request.FILES['image']
             item.restaurant_memo = regex_format_space(
                 request.POST['restaurant_memo'])
             item.restaurant_name = regex_format_space(
@@ -185,6 +184,34 @@ class ItemCreateView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(self.success_url)
 
 
+    def post(self, request, *args, **kwargs):
+        postForm = PostForm(request.POST)
+        lis = request.FILES.getlist('image')
+        image_form = ImageForm(request.FILES)
+        if postForm.is_valid():
+            print("valid~~~")
+            item = postForm.save(commit=False)
+            item.restaurant_memo = regex_format_space(
+                request.POST['restaurant_memo'])
+            item.restaurant_name = regex_format_space(
+                request.POST['restaurant_name'])
+            item.created_by = self.request.user
+            item.created_at = timezone.now()
+            item.updated_by = self.request.user
+            item.updated_at = timezone.now()
+            item.save()
+            if image_form.is_valid:
+                for image in lis:
+                    image_instance = Images(
+                        image=image, item=item
+                    )
+                    image_instance.save()
+            messages.success(request, f'投稿ありがとうございます!')
+            return HttpResponseRedirect(self.success_url)
+
+        return render(request, "/", {'form': postForm})
+
+
 class ItemUpdateView(LoginRequiredMixin, UpdateView):
     """
     ビュー：更新画面
@@ -240,7 +267,7 @@ class CardDetailPageView(DetailView):
 
         context = super().get_context_data(**kwargs)
 
-        images = Images.objects.filter(item_id=context["item"].id).all()
+        images = Images.objects.filter(item_id=context["item"].id).order_by('id').reverse().all()
 
         context["images"] = images
         context["show_postbutton"] = False
