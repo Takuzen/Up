8dac8d4144d7ab03fc797fcdeee0e326a95a2e9b
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-09-13 09:09:02 +0900

    card detailログアウトしているときにもアクセスできるように

--- up/app/views.py
+++ up/app/views.py
@@ -282,7 +282,10 @@ class CardDetailPageView(DetailView):
         context["comments"] = Comment.objects.filter(
             item_id=context['object'].id).order_by('commented_date').reverse()
         followee_id = Item.objects.get(id=context["object"].id).created_by_id
-        follower_id = User.objects.get(id=self.request.user.id).id
+        try:
+            follower_id = User.objects.get(id=self.request.user.id).id
+        except:
+            follower_id = followee_id
 
         if follower_id != followee_id:
             print("different id")
