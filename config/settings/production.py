19193ea02f4a56310f0e52d8096b348b653d42cc
takuzen <takuzen0430@gmail.com>
2020-09-19 20:05:34 +0900

    added staging environment's url to allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = [
     'up-env.eba-aesibyxa.us-east-1.elasticbeanstalk.com',
     'www.uplife.link',
     'uplife.link',
-    'up-staging.us-east-1.elasticbeanstalk.com',
+    'up-env-b.us-east-1.elasticbeanstalk.com',
 ]
 
 DATABASES = {
